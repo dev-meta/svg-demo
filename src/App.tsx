d66cf1e861fe86fc25399d3d89a1dd5eb773a766
wangzongqi01 <wangzongqi01@baidu.com>
2022-07-12 23:49:20 +0800

    add react-app-env.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 function App() {
     return <div>
-        {/* <img src='/ring.png' alt="" /> */}
         <h1>欢迎使用react模板</h1>
     </div>
 }
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'production' | 'development';
+    }
+}
